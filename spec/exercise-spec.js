a350fed602b7cdfd2ee59d5fcfc7c543af0fb873
Matthew Murgia <matthew.murgia@gmail.com>
2015-12-03 13:49:35 -0500

    updated test spec

--- spec/exercise-spec.js
+++ spec/exercise-spec.js
@@ -2,13 +2,23 @@
 
 describe("an exercise", function(){
   it("should have a name", function(){
+    var running = new Exercise("Running");
+    expect (running.name).toBeDefined();
   });
+
   it("should have a description", function(){
+    var running = new Exercise("Running");
+    expect (running.description).toBeDefined();
   });
+  
   it("should have a video", function(){
   });
+    var running = new Exercise("Running");
+    expect (running.video).toBeDefined();
+
   it("should have an edit button", function(){
   });
+
   it("should have a delete button", function(){
   });
 });
