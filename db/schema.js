5a984fc2d243527cca71b60266d2951394d65eb8
Matthew Murgia <matthew.murgia@gmail.com>
2015-12-03 14:50:24 -0500

    schema complete

--- db/schema.js
+++ db/schema.js
@@ -2,5 +2,29 @@
 var mongoose = require('mongoose');
 
 // instantiate a name space for our Schema constructor defined by mongoose.
-var Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var WorkoutSchema = new Schema(
+  {
+    title: String,
+    exercise:[{type: ObjectId, ref: "Exercise"}]
+  },
+  {
+    toObject: {virtuals: true},
+    toJSON: {virtuals: true}
+  }
+);
+
+WorkoutSchema.virtual("id").get(function(){
+  return this._id;
+});
+
+var ExerciseSchema = new Schema({
+  name: String,
+  description: String,
+  video_url: String
+});
+
+var WorkoutModel = mongoose.model("Workout", WorkoutSchema);
+var ExerciseModel = mongoose.model("Exercise", ExerciseSchema);
