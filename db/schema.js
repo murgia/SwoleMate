de54f7ab93e19dd79daaa68bad13c05c6812bab9
Ly Nguyen <Ly900@aol.com>
2015-12-04 16:25:57 -0500

    fix schema and get it working; update workouts controller for get workouts and corresponding exercises; start work on exercises controller

--- db/schema.js
+++ db/schema.js
@@ -2,28 +2,28 @@ var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/swolemate");
 
 var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
+var ObjectId = Schema.Types.ObjectId;
 
 var WorkoutSchema = new Schema(
   {
     title: String,
-    exercise:[{type: ObjectId, ref: "Exercise"}]
+    exercises:[{type: ObjectId, ref: "Exercise"}]
   },
   {
     toObject: {virtuals: true},
     toJSON: {virtuals: true}
   }
 );
-//
-// WorkoutSchema.virtual("id").get(function(){
-//   return this._id;
-// });
+
+WorkoutSchema.virtual("id").get(function(){
+  return this._id;
+});
 
 var ExerciseSchema = new Schema({
   name: String,
   description: String,
   video_url: String,
-  workout:[{type: ObjectId, ref: "Workout"}],
+  workout:{type: ObjectId, ref: "Workout"},
   sets: Number,
   reps: Number
 });
