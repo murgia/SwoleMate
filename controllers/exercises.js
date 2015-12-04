4c09fd0d2d97a033c2b88420f82c85efa9e37154
Ly Nguyen <Ly900@aol.com>
2015-12-04 17:32:54 -0500

    update exercise controller

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -14,30 +14,5 @@ router.get("/", function(req, res){
   });
 });
 // above route is correct
-// check the routes below
-
-router.get("/:id", function(req, res){
-  exercise.findById(req.params.id).populate("workout", "name").then(function(exercise){
-    res.json(exercise);
-  });
-});
-
-router.put("/:id", function(req, res){
-  exercise.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true}).then(function(exercise){
-    res.json(exercise);
-  });
-});
- 
-router.delete("/:id", function(req, res){
-  exercise.findById(req.params.id).then(function(exercise){
-    workout.findByIdAndUpdate(exercise.workout._id, {
-      $pull: { exercises: {_id: req.params.id} }
-    }).then(function(){
-      return exercise.remove();
-    }).then(function(){
-      res.json({success: true});
-    });
-  });
-});
 
 module.exports = router;
