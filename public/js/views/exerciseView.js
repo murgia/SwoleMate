cded57933d3f68f98a9d59f11e7a9037c69201d4
Ly Nguyen <Ly900@aol.com>
2015-12-09 11:35:50 -0500

    working on edit exercises

--- public/js/views/exerciseView.js
+++ public/js/views/exerciseView.js
@@ -5,6 +5,7 @@ var ExerciseView = function(exercise){
 ExerciseView.prototype = {
   // renders the exercise view as a bootstrap
   render: function(){
+    var self = this;
     var el =
     $("<div class='col-sm-6 col-md-4'>\
     <div class='caption exercise-div card-title'>\
@@ -15,12 +16,19 @@ ExerciseView.prototype = {
     <p><strong>Sets:</strong> " + this.exercise.sets + "</p>\
     <p><strong>Reps: </strong>" + this.exercise.reps + "</p>\
     <p><strong>Video Url: </strong>" + this.exercise.video_url + "</p>\
-    <a href='#' class='btn btn-default' role='button'>Edit Exercise</a>\
+    <button class='btn btn-default editExercise'>Edit Exercise</button>\
     <a href='#' class='btn btn-default' role='button'>Delete Exercise</a>\
     <input type='checkbox'>\
     </div>\
     </div>");
-    // $("<p>Name: " + this.exercise.name + "</p>" + "<p>Description: " + this.exercise.description + "</p>" + "<p>Sets: " + this.exercise.sets + "</p>" + "<p>Reps: " + this.exercise.reps + "</p>" + "<p>Video Demo: " + this.exercise.video_url + "</p>");
-    return(el)
+    console.log(self);
+    return(el);
+    var editButton = self.find("button.editExercise");
+    editButton.on("click", function(){
+      console.log("Edit form will be rendered")
+    //   self.renderEditForm();
+    });
+
+
   }
 }
