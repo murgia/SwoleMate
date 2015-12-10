4fcdfed1423d5a69cafb892008578c2dfa6fa005
Ly Nguyen <Ly900@aol.com>
2015-12-10 15:19:50 -0500

    fix wunderground api code - had state and city fields mixed up

--- public/js/script.js
+++ public/js/script.js
@@ -10,17 +10,17 @@ $(document).ready(function(){
 
   $("button#check-weather").on("click", function(evt){
     evt.preventDefault();
-      var city = $("input#state").val();
-      var state = $("input#city").val();
-     console.log(city);
+      var state = $("input#state").val();
+      var city = $("input#city").val();
      console.log(state);
-      var url = "http://api.wunderground.com/api/14da815aa9cb1bfe/conditions/q/"+state+"/"+city+".json"
+     console.log(city);
+      var url = "http://api.wunderground.com/api/14da815aa9cb1bfe/conditions/q/" + state + "/" + city + ".json"
       $.ajax({
         url: url,
         type: "get",
         dataType: "json"
       }).done(function(response){
-        $("div#weather").append(
+        $("div#weather").html(
           "<ul>\
             <li class='text-center'><strong><h4>Temperature: </strong>" + response.current_observation.temp_f + "&deg; F</h4>" +
             "<li class='text-center'><strong><h4>Weather: </strong>" + response.current_observation.weather + "</h4></li>" +
@@ -29,7 +29,7 @@ $(document).ready(function(){
         }).fail(function(){
         console.log("Ajax request failed.");
       }).always(function(){
-        console.log("Ajax request successful!"); 
+        console.log("Ajax request successful!");
       });
     });
 
